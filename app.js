cbed88da09892eb2b30475bd97a8ad39fa470cd6
Jeremy Iglehart <jeremy@autoschematic.com>
2015-11-29 08:02:20 -0500

    who do voodoo?
    you do voodoo?

--- app.js
+++ app.js
@@ -2,14 +2,20 @@
   var app = angular.module('store',[]);
 
   app.controller('StoreController', function(){
-    this.product = gem;
+    this.product = gems;
   });
 
-  var gem = {
+  var gems = [
+  {
     name: 'Docecahdron',
     price: 2.95,
     description: 'This file changes and so does the output :D',
     canPurchase: false,
-    soldOut: false,
+  },
+  {
+    name: 'Pentagonal Gem',
+    price: 5.95,
+    description: '. . .',
+    canPurchase: false,
   };
 })();
